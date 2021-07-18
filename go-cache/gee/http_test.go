7cc7d9f04952579c56b5abd3643314f73a0c8efb
tsingluo@tencent.com <tsing@tencent.com>
2021-07-18 10:58:36 +0800

    file sinker

--- go-cache/gee/http_test.go
+++ go-cache/gee/http_test.go
@@ -10,19 +10,20 @@ import(
 // https://geektutu.com/post/quick-gomock.html
 // 
 
-func startCacheServer(addr string, svrs []string) {
+func startCacheServer(addr string, svrs []string, sinker Sinker) {
 	pool := NewHttpPool(addr, 5, svrs)
-	sinker := NewSMap()
+	
 	name := "testg"
 	g := NewGroup(name, 2<<10, sinker) // sinker 本地内存， name是内存的名称
 	g.RegisterPeers(pool)
 	pool.Run()
 
 }
-var addr string
+var addr, sinker string
 func init() {
 	testing.Init()
 	flag.StringVar(&addr, "server", "localhost:8090", "cache server")
+	flag.StringVar(&sinker, "sinker", "map", "cache sinker")
 	flag.Parse()
 }
 // set curl -i -X POST "http://localhost:8092/api/set/testg/key" -d "value"
@@ -31,14 +32,23 @@ func TestGroupCache(t *testing.T){
 
 	servers := []string{"localhost:8089", "localhost:8090", "localhost:8091", "localhost:8092", "localhost:8088"}
 	fmt.Printf("start addr:%s\n", addr)
-	startCacheServer(addr, servers)
+
+	var getter Sinker
+	if sinker == "map" {
+		getter = NewSMap()
+	} else if sinker == "file" {
+		getter = NewFileSinker("./")
+	} else {
+		t.Fatalf("error sinker:%s! sinker should be map or file!", sinker)
+	}
+	startCacheServer(addr, servers, getter)
 	/*
 	for i, addr := range servers{
 		name := fmt.Sprintf("server %d", i)
 		addr := addr
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
-			startCacheServer(addr, servers) // 这样测试是不行的，因为有一个groups的全局的
+			startCacheServer(addr, servers) // 这样测试是不行的，因为有一个groups的全局的变量
 		})
 		
 	}*/
--- go-cache/gee/sink.go
+++ go-cache/gee/sink.go
@@ -1,6 +1,8 @@
 package gee
 import(
 	"fmt"
+	"os"
+	"io/ioutil"
 )
 
 // 也测试下FileSinker
@@ -33,3 +35,38 @@ func (s *SMap)Get(key string) ([]byte, error) {
 	return value, nil
 }
 
+type FileSinker struct {
+	dataPath string
+}
+
+func NewFileSinker(dataPath string) *FileSinker {
+	return &FileSinker{
+		dataPath: dataPath,
+	}
+}
+
+func (s *FileSinker)Set(key string, value []byte) error {
+	file := s.dataPath + "/" + key
+	f, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	_, err = f.Write(value)
+	if err != nil {
+		return err
+	}
+	
+	// 按照key写文件
+	return nil
+}
+
+func (s *FileSinker)Get(key string) ([]byte, error) {
+	// 按照文件读取
+	file := s.dataPath + "/" + key
+	f, err := os.Open(file)
+	value, err := ioutil.ReadAll(f)
+	if err != nil {
+		return value, err
+	}
+	return value, nil
+}
